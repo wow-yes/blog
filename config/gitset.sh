2524dbc67d60e32512516e482e1f4bbc99225115
lpb@t5810 <lipengbo@msn.com>
2019-09-02 21:53:13 +0800

    Mon Sep  2 21:53:13 CST 2019

--- config/gitset.sh
+++ config/gitset.sh
@@ -5,6 +5,7 @@ git config --global core.quotepath false # 显示 status 编码
 git config --global gui.encoding utf-8 # 图形界面编码 
 git config --global i18n.commit.encoding utf-8 # 提交说明编码 
 git config --global i18n.logoutputencoding utf-8
+git config --global core.filemode false
 # 作者：bill_20106029 
 # 来源：CSDN 
 # 原文：https://blog.csdn.net/elegant__/article/details/76180154 
