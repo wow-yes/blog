cf13a1cf79c9e4de29a6293c2478e3bd635286c1
lpb@t5810 <lipengbo@msn.com>
2019-10-04 12:14:57 +0800

    Fri Oct  4 12:14:57 CST 2019

--- config/link.sh
+++ config/link.sh
@@ -3,10 +3,17 @@ pwd=`cd $(dirname $0);pwd -P`
 ln -fs $(pwd)/_vimrc ~/.vimrc
 ln -sf $(pwd)/bashrc ~/.bashrc
 ln -sf $(pwd)/Xresources ~/.Xresources
+
+rm -rf ~/.desk.png
+rm -rf ~/.lock.png
+ln -sf $(pwd)/desk.png ~/.desk.png
+ln -sf $(pwd)/lock.png ~/.lock.png
+
 rm -rf ~/.i3*
 mkdir ~/.i3/
 ln -sf $(pwd)/config ~/.i3/config
 ln -sf $(pwd)/ctags.conf ~/.ctags
+
 rm -rf ~/.cache
 ln -sf /dev/shm/ ~/.cache
 rm -rf ~/.adobe
