93eb6d423d6215897abfd626f944c573ecbdad1a
lpb@t5810 <lipengbo@msn.com>
2019-12-10 21:35:09 +0800

    Tue Dec 10 21:35:09 CST 2019

--- config/installMyApp.sh
+++ config/installMyApp.sh
@@ -1,18 +1,21 @@
 
-sudo echo "deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse"> /etc/apt/sources.list
-sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse">> /etc/apt/sources.list
-sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse">> /etc/apt/sources.list
-sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
+#sudo echo "deb http://mirrors.163.com/ubuntu/ bionic main restricted universe multiverse"> /etc/apt/sources.list
+#sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-security main restricted universe multiverse">> /etc/apt/sources.list
+#sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-updates main restricted universe multiverse" >> /etc/apt/sources.list
+#sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-proposed main restricted universe multiverse">> /etc/apt/sources.list
+#sudo echo "deb http://mirrors.163.com/ubuntu/ bionic-backports main restricted universe multiverse" >> /etc/apt/sources.list
 
-sudo apt-get clean
-sudo apt-get dist-upgrade 
+#sudo apt-get clean
+#sudo apt-get dist-upgrade 
 
 # install program software
-sudo apt-get install vim git gcc  build-essential gfortran
 # install 
-sudo apt-get install i3-wm i3lock-fancy redshift-gtk feh firefox pcmanfm lxappearance lxterminal
-sudo apt-get install wicd-gtk flameshot nomacs simplescreenrecorder smplayer bleachbit gparted
+sudo apt-get install i3-wm i3lock-fancy redshift-gtk feh firefox pcmanfm \ 
+    vim git gcc  build-essential gfortran \
+    lxappearance lxterminal gnome-disk-utility flameshot nomacs \
+    simplescreenrecorder smplayer bleachbit gparted gtkorphan i3status \
+    i3blocks blueman rofi  lxappearance
+
 sudo apt-get autoremove
 #
 #git clone 
