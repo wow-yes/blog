b89ac55f0dbf78d32185d032371e320ae24f3253
lipengbo <lipengbo@msn.com>
2021-03-26 08:53:46 +0800

    `date`

--- config/installMyApp.sh
+++ config/installMyApp.sh
@@ -14,7 +14,7 @@ sudo apt-get dist-upgrade
 #
 sudo apt-get install vim git gcc  build-essential gfortran gdb python3 \
     bash-completion python3-pip pandoc pandoc-citeproc tmux wget axel \
-    exuberant-ctags tig ffmpeg tcl-dev tk-dev python3-tk
+    exuberant-ctags tig ffmpeg tcl-dev tk-dev python3-tk graphviz
 
 #sudo apt-get install nomacs pcmanfm
 
@@ -22,7 +22,7 @@ sudo apt-get install vim git gcc  build-essential gfortran gdb python3 \
 pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 pip3 install -U pip
 #https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install numpy matplotlib pandas ipython scipy tk keras tensorflow
+pip3 install numpy matplotlib pandas ipython scipy tk keras tensorflow pydot
 
 sudo apt-get autoremove
 sudo apt-get clean
