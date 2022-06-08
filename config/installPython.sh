b3fcd57820521dbf46606ffab48facf2aea203a4
lipengbo <lipengbo@msn.com>
2022-06-08 09:57:19 +0800

    delete

--- config/installPython.sh
+++
@@ -1,4 +0,0 @@
-pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
-pip3 install -U pip
-pip3 install -U numpy matplotlib pandas ipython scipy tk cartopy
-pip3 install -U keras tensorflow pydot sklearn pyforest geopandas
