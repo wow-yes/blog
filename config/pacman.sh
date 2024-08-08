386c3a09a67905e9199dae36296a924dc5316fca
Your Name <you@example.com>
2024-08-08 23:16:49 +0800

    pacman.sh

---
+++ config/pacman.sh
@@ -0,0 +1,12 @@
+
+pacman -S base-devel mingw-w64-ucrt-x86_64-toolchain
+
+mingw-w64-ucrt-x86_64-python-numpy
+mingw-w64-ucrt-x86_64-python-pandas
+mingw-w64-ucrt-x86_64-python-matplotlib
+mingw-w64-ucrt-x86_64-python-numpy
+
+wget axel
+p7zip unzip
+
+
