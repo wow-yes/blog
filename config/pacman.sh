70a0dea5991abe894cb172d078a4a5f15891bb32
lipengbo <lipengbo@msn.com>
2025-05-21 16:34:40 +0800

    update

--- config/pacman.sh
+++ config/pacman.sh
@@ -1,12 +1,4 @@
 
 pacman -S base-devel mingw-w64-ucrt-x86_64-toolchain
 
-mingw-w64-ucrt-x86_64-python-numpy
-mingw-w64-ucrt-x86_64-python-pandas
-mingw-w64-ucrt-x86_64-python-matplotlib
-mingw-w64-ucrt-x86_64-python-numpy
-
-wget axel
-p7zip unzip
-
-
+pacman -S parallel wget axel p7zip unzip git vim 
