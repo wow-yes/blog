cac14dbead124a0ae2998bfb07f27a68082bef1a
lipengbo <lipengbo@msn.com>
2025-12-30 10:42:44 +0800

    date

--- config/install_Jekyll.sh
+++ config/install_Jekyll.sh
@@ -1,4 +1,6 @@
 sudo apt-get install ruby-full build-essential zlib1g-dev
-gem install jekyll bundler
+gem sources --remove https://rubygems.org/  # Remove default source
+gem sources -a https://mirrors.aliyun.com/rubygems/ # Add Aliyun mirror source
+sudo gem install jekyll bundler
 bundle install
 bundle exec jekyll serve
