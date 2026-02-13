6ca9de79443288272848f34966f347ad60656d5b
lipengbo <lipengbo@msn.com>
2026-02-13 15:52:25 +0800

    feat(pacman): add pacman script

--- config/pacman.sh
+++ config/pacman.sh
@@ -1,34 +1,27 @@
-#!/bin/bash
-#pacman -S --needed base-devel parallel wget axel p7zip unzip vim rsync
-#
-## 在 UCRT64 终端中运行
-#pacman -S --needed mingw-w64-ucrt-x86_64-toolchain mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-python mingw-w64-ucrt-x86_64-python-pip
-## 开发库
-#pacman -S --needed mingw-w64-ucrt-x86_64-lapack mingw-w64-ucrt-x86_64-openblas mingw-w64-ucrt-x86_64-fftw mingw-w64-ucrt-x86_64-libjpeg-turbo mingw-w64-ucrt-x86_64-libpng mingw-w64-ucrt-x86_64-libtiff
-#
-#pacman -S --needed mingw-w64-ucrt-x86_64-python-numpy mingw-w64-ucrt-x86_64-python-matplotlib mingw-w64-ucrt-x86_64-python-pandas mingw-w64-ucrt-x86_64-python-ipython mingw-w64-ucrt-x86_64-python-scipy mingw-w64-ucrt-x86_64-python-pygments
-
-# 安装 UCRT 工具链
-pacman -S --needed base-devel
-pacman -S --needed mingw-w64-ucrt-x86_64-toolchain
-
-# 或单独安装组件
-pacman -S --needed mingw-w64-ucrt-x86_64-gcc      # GCC 编译器
-pacman -S --needed mingw-w64-ucrt-x86_64-gdb      # 调试器
-pacman -S --needed mingw-w64-ucrt-x86_64-make     # Make 工具
-pacman -S --needed mingw-w64-ucrt-x86_64-cmake    # CMake
-pacman -S --needed mingw-w64-ucrt-x86_64-binutils # 二进制工具
-
-# 代码编辑和工具
-pacman -S --needed mingw-w64-ucrt-x86_64-vim
-#pacman -S --needed mingw-w64-ucrt-x86_64-nano
-#pacman -S --needed git
-
-# 构建工具
-pacman -S --needed mingw-w64-ucrt-x86_64-ninja
-pacman -S --needed mingw-w64-ucrt-x86_64-meson
-
-# 调试和分析工具
-pacman -S --needed mingw-w64-ucrt-x86_64-valgrind
-pacman -S --needed mingw-w64-ucrt-x86_64-strace
-pacman -S --needed mingw-w64-ucrt-x86_64-ltrace
+pacman -S mingw-w64-ucrt-x86_64-clang #21.1.8-4
+pacman -S mingw-w64-ucrt-x86_64-cppcheck #2.19.1-1
+pacman -S mingw-w64-ucrt-x86_64-crt #13.0.0.r505.g7d006b2ea-1
+pacman -S mingw-w64-ucrt-x86_64-doxygen #1.16.1-1
+pacman -S mingw-w64-ucrt-x86_64-fzf #0.67.0-1
+pacman -S mingw-w64-ucrt-x86_64-gcc #15.2.0-11
+pacman -S mingw-w64-ucrt-x86_64-gdb #17.1-3
+pacman -S mingw-w64-ucrt-x86_64-gdb-multiarch #17.1-3
+pacman -S mingw-w64-ucrt-x86_64-graphviz #14.1.1-3
+pacman -S mingw-w64-ucrt-x86_64-headers #13.0.0.r505.g7d006b2ea-1
+pacman -S mingw-w64-ucrt-x86_64-libmangle #13.0.0.r505.g7d006b2ea-1
+pacman -S mingw-w64-ucrt-x86_64-libwinpthread #13.0.0.r505.g7d006b2ea-1
+pacman -S mingw-w64-ucrt-x86_64-make #4.4.1-4
+pacman -S mingw-w64-ucrt-x86_64-openblas #0.3.31-1
+pacman -S mingw-w64-ucrt-x86_64-pkgconf #1~2.5.1-1
+pacman -S mingw-w64-ucrt-x86_64-python-matplotlib #3.10.8-2
+pacman -S mingw-w64-ucrt-x86_64-python-numpy #2.4.1-2
+pacman -S mingw-w64-ucrt-x86_64-python-pandas #2.3.3-2
+pacman -S mingw-w64-ucrt-x86_64-python-pygments #2.19.2-4
+pacman -S mingw-w64-ucrt-x86_64-python-scipy #1.17.0-2
+pacman -S mingw-w64-ucrt-x86_64-qhull #2020.2-3
+pacman -S mingw-w64-ucrt-x86_64-ripgrep #15.1.0-1
+pacman -S mingw-w64-ucrt-x86_64-tools #13.0.0.r505.g7d006b2ea-1
+pacman -S mingw-w64-ucrt-x86_64-winpthreads #13.0.0.r505.g7d006b2ea-1
+pacman -S mingw-w64-ucrt-x86_64-winstorecompat #13.0.0.r505.g7d006b2ea-1
+pacman -S mingw-w64-x86_64-python-black #26.1.0-2
+pacman -S ncdu neofetch openssh parallel procps-ng rsync tmux tree unzip vim axel p7zip unzip
