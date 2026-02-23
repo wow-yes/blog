1c1d4b3315ed96daeb79aa0383e213ac57572080
lipengbo <lipengbo@msn.com>
2026-02-23 14:04:25 +0800

    add tmux conf

--- config/pacman.sh
+++ config/pacman.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 pacman -S --needed mingw-w64-ucrt-x86_64-clang #21.1.8-4
 pacman -S --needed mingw-w64-ucrt-x86_64-cppcheck #2.19.1-1
-pacman -S --needed mingw-w64-ucrt-x86_64-crt #13.0.0.r505.g7d006b2ea-1
 pacman -S --needed mingw-w64-ucrt-x86_64-doxygen #1.16.1-1
 pacman -S --needed mingw-w64-ucrt-x86_64-fzf #0.67.0-1
 pacman -S --needed mingw-w64-ucrt-x86_64-gcc #15.2.0-11
 pacman -S --needed mingw-w64-ucrt-x86_64-gdb #17.1-3
-pacman -S --needed mingw-w64-ucrt-x86_64-gdb-multiarch #17.1-3
 pacman -S --needed mingw-w64-ucrt-x86_64-graphviz #14.1.1-3
-pacman -S --needed mingw-w64-ucrt-x86_64-headers #13.0.0.r505.g7d006b2ea-1
-pacman -S --needed mingw-w64-ucrt-x86_64-libmangle #13.0.0.r505.g7d006b2ea-1
 pacman -S --needed mingw-w64-ucrt-x86_64-libwinpthread #13.0.0.r505.g7d006b2ea-1
 pacman -S --needed mingw-w64-ucrt-x86_64-make #4.4.1-4
 pacman -S --needed mingw-w64-ucrt-x86_64-openblas #0.3.31-1
-pacman -S --needed mingw-w64-ucrt-x86_64-pkgconf #1~2.5.1-1
 pacman -S --needed mingw-w64-ucrt-x86_64-python-matplotlib #3.10.8-2
 pacman -S --needed mingw-w64-ucrt-x86_64-python-numpy #2.4.1-2
 pacman -S --needed mingw-w64-ucrt-x86_64-python-pandas #2.3.3-2
-pacman -S --needed mingw-w64-ucrt-x86_64-python-pygments #2.19.2-4
 pacman -S --needed mingw-w64-ucrt-x86_64-python-scipy #1.17.0-2
 pacman -S --needed mingw-w64-ucrt-x86_64-qhull #2020.2-3
 pacman -S --needed mingw-w64-ucrt-x86_64-ripgrep #15.1.0-1
@@ -26,5 +20,5 @@ pacman -S --needed mingw-w64-ucrt-x86_64-winpthreads #13.0.0.r505.g7d006b2ea-1
 pacman -S --needed mingw-w64-ucrt-x86_64-winstorecompat #13.0.0.r505.g7d006b2ea-1
 pacman -S --needed mingw-w64-ucrt-x86_64-global
 pacman -S --needed mingw-w64-ucrt-x86_64-python-black
-
-pacman -S --needed ncdu neofetch openssh parallel procps-ng rsync tmux tree unzip vim axel p7zip unzip
+pacman -S --needed mingw-w64-ucrt-x86_64-ripgrep mingw-w64-ucrt-x86_64-cmake
+pacman -S --needed ncdu neofetch openssh parallel procps-ng rsync tmux tree unzip vim axel p7zip unzip tig git
