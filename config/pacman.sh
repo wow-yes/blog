6f7e377fa0fc5b27bb03d3e4aafe13f9e17ec51e
lipengbo <lipengbo@msn.com>
2026-02-26 16:43:35 +0800

    feat(tmux): new theme

--- config/pacman.sh
+++ config/pacman.sh
@@ -20,4 +20,4 @@ pacman -S --needed mingw-w64-ucrt-x86_64-winpthreads #13.0.0.r505.g7d006b2ea-1
 pacman -S --needed mingw-w64-ucrt-x86_64-winstorecompat #13.0.0.r505.g7d006b2ea-1
 pacman -S --needed mingw-w64-ucrt-x86_64-python-black
 pacman -S --needed mingw-w64-ucrt-x86_64-ripgrep mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-bat mingw-w64-ucrt-x86_64-ctags 
-pacman -S --needed ncdu neofetch openssh parallel procps-ng rsync tmux tree unzip vim axel p7zip unzip tig git make
+pacman -S --needed ncdu neofetch openssh parallel procps-ng rsync tmux tree unzip vim axel p7zip unzip tig git make colordiff
