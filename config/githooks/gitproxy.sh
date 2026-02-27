f4b944ada85b031b4245c6845dfcd92d29ab7261
lipengbo <lipengbo@msn.com>
2026-02-27 20:42:15 +0800

    feat(git): fix gitporxy.sh

--- config/githooks/gitproxy.sh
+++ config/githooks/gitproxy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+# 取消当前仓库的代理
+git config --unset http.proxy
+git config --unset https.proxy
 # 如果代理是 http 类型 (如 Clash 默认)
-git config --global http.proxy http：//127.0.0.1：7890
-git config --global https.proxy http：//127.0.0.1：7890
+git config --global http.proxy http://127.0.0.1:7890
+git config --global https.proxy http://127.0.0.1:7890
 
 # 如果代理是 socks5 类型 (如某些机场)
-# git config --global http.proxy socks5：//127.0.0.1：7890
-# git config --global https.proxy socks5：//127.0.0.1：7890
+# git config --global http.proxy socks5://127.0.0.1:7890
+# git config --global https.proxy socks5://127.0.0.1:7890
 
 # 查看当前仓库的代理设置
 git config --local --list | grep proxy
 
-# 取消当前仓库的代理
-git config --unset http.proxy
-git config --unset https.proxy
